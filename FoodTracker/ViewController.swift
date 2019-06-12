c1ec88a2072007ef3d0afc13eeb7f9f6958db458
SamuelMuloki <samuellebron0@gmail.com>
2019-06-12 18:08:36 +0300

    [New] Add view for rating control

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,9 +33,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    // MARK: UIImagePickerContollerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You may pick the original one.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image but was provided with the following \(info)")
+        }
+        // Set photoImageView to display the selected image
+        photoImageView.image = selectedImage
+        // dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: Actions
-
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hides the keyboard
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from the photo library
+        let imagePickerController = UIImagePickerController()
+        // Only allows photos to be picked not taken
+        imagePickerController.sourceType = .photoLibrary
+        // Mak sure the view contoller is notified when th user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
