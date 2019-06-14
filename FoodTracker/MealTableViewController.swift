20caa8449dcc86d9e091c5fdb9cf4028898b429a
SamuelMuloki <samuellebron0@gmail.com>
2019-06-14 18:49:33 +0300

    [New] Add meal table view controller

---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by samuel muloki on 14/06/2019.
+//  Copyright © 2019 line23. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    // MARK: Properties
+    
+    var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //load sample data
+        loadSampleMeals()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier
+        let cellIdentifier = "MealTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        // Fetches the appropriate meal from the data source layout
+        let meal = meals[indexPath.row]
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: Private methods
+    
+    private func loadSampleMeals() {
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "Vegetables", photo: photo1, rating: 4) else {
+            fatalError("Unable to instatiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Whole chicken", photo: photo2, rating: 5) else {
+            fatalError("Unable to instatiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Spicy wings", photo: photo3, rating: 3) else {
+            fatalError("Unable to instatiate meal3")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+
+}
